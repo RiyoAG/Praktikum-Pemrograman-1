7a1fb3f84a5d01a0db6ee1770fb44ad132c5df1d
RiyoAG <115161952+RiyoAG@users.noreply.github.com>
2022-10-15 18:55:17 +0800

    Add files via upload

---
+++ Modul 1/NO 4/PRAK004-2210817310016-Riyo Aurora Guison.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+printf("#############################\n");
+printf("#                           #\n");
+printf("#    Riyo Aurora Guison     #\n");
+printf("#    2210817310016          #\n");
+printf("#                           #\n");
+printf("#############################\n");
+}

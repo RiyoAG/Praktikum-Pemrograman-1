4780b0872437a0eaa8dd4bf461f0bccd74aac205
RiyoAG <115161952+RiyoAG@users.noreply.github.com>
2022-10-30 20:21:38 +0800

    .

--- Modul4/NO 1/PRAK301-2210817310016-Riyo Aurora Guison.py
+++
@@ -1,7 +0,0 @@
-x,y = input().split ()
-x=int(x)
-y=int(y)
-if(x<y):
-    print(x,y)
-elif(y<x):
-    print(y,x)
--- Modul4/NO 3/PRAK303-2210817310016-Riyo Aurora Guison.py
+++
@@ -1,7 +0,0 @@
-Nilai=int(input(""))
-if(Nilai > 0):
-    print("positif")
-elif(Nilai < 0):
-    print("negatif")
-elif(Nilai == 0):
-    print('nol')
